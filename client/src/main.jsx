5bd019e62164781d9f292f830170b248a2ebef50
Oleksandr Ryzhyk <16495253+sancho1x@users.noreply.github.com>
2026-05-27 03:24:38 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
+import { SpeedInsights } from '@vercel/speed-insights/react' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
